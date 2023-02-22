3ee6f717b8956a50abfab356a4cb7fbf1cc7caab
rfajri27 <rfajri255@gmail.com>
2023-02-22 20:56:28 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # doubleF-deployment
+
+## Flask Deployment
+
+Run the flask app by running this command
+
+* For Windows CMD
+
+    ```
+    set FLASK_APP=main.py
+    flask run
+    ```
+
+* For PowerShell
+
+    ```PowerShell
+    $env:FLASK_APP="main.py"
+    flask run
+    ```
+
+* For Linux Terminal
+
+    ```bash
+    export FLASK_APP=main.py
+    flask run
+    ```
+
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import os
 import json
 import joblib
 from flask import Flask, request
+
+MODEL_ROOT = "model"
+main_root = os.getcwd()
+
+# Load the model & all the dependencies
+model = joblib.load("log_model.joblib")
+scaler = joblib.load("\model\StandardScaler.joblib")
+le_gender = joblib.load("\model\LabelEncodergender.joblib")
+le_InternetService = joblib.load("\model\LabelEncoderInternetService.joblib")
+le_PaperlessBilling = joblib.load("\model\LabelEncoderPaperlessBilling.joblib")
+le_Partner = joblib.load("\model\LabelEncoderPartner.joblib")
+le_PhoneService = joblib.load("\model\LabelEncoderPhoneService.joblib")
+le_SeniorCitizen = joblib.load("\model\LabelEncoderSeniorCitizen.joblib")
+le_StreamingTV = joblib.load("\model\LabelEncoderStreamingTV.joblib")
 
 app = Flask(__name__)
 
@@ -10,29 +25,39 @@ def hello_world():
     return "Hello, World!"
 
 
-# Load the model
-model = joblib.load("model\log_model.joblib")
-scaler = joblib.load("model\StandardScaler.joblib")
-label_encoder = joblib.load("model\LabelEncoder.joblib")
-
 def preprocessing(data):
-    data[:7] = label_encoder.transform(data[:7])
-    data[-3:] = scaler.transform(data[-3:])
+    new_data = []
+    numeric_data = []
     
-    return data
+    new_data.append(le_gender.transform([data["gender"]])[0])
+    new_data.append(le_SeniorCitizen.transform([data["SeniorCitizen"]])[0])
+    new_data.append(le_Partner.transform([data["Partner"]])[0])
+    new_data.append(le_StreamingTV.transform([data["StreamingTV"]])[0])
+    new_data.append(le_PhoneService.transform([data["PhoneService"]])[0])
+    new_data.append(le_InternetService.transform([data["InternetService"]])[0])
+    new_data.append(le_PaperlessBilling.transform([data["PaperlessBilling"]])[0])
+    
+    numeric_data.append(data["MonthlyCharges"])
+    numeric_data.append(data["TotalCharges"])
+    numeric_data.append(data["tenure"])
+    new_numeric_data = scaler.transform([numeric_data])[0]
+    new_data.append(new_numeric_data[0])
+    new_data.append(new_numeric_data[1])
+    new_data.append(new_numeric_data[2])
+    return new_data
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
     request_json = request.json
-    data = preprocessing(request_json.get("data"))
+    raw_data = request_json.get("data")
+    data_preprocessing = preprocessing(raw_data)
 
-    prediction = model.predict(data)
+    prediction = model.predict([data_preprocessing])
     prediction_string = [str(d) for d in prediction]
 
     response_json = {
-        "raw_data": request_json.get("data"),
-        "data": data,
+        "raw_data": raw_data,
         "prediction": list(prediction_string)
     }
 
